a90e276f20746a6ddfdc684c264d5c67355cdca1
Bhoomika Kapde <165261127+bhumii2205@users.noreply.github.com>
2025-03-04 23:07:44 +0530

    Add files via upload
    Add Student.java

---
+++ Student.java
@@ -0,0 +1,55 @@
+// Student.java
+// Defines the Student class with attributes and methods
+
+public class Student {
+    private String prn;
+    private String name;
+    private String dob;
+    private double marks;
+
+    // Constructor
+    public Student(String prn, String name, String dob, double marks) {
+        this.prn = prn;
+        this.name = name;
+        this.dob = dob;
+        this.marks = marks;
+    }
+
+    // Getters and Setters
+    public String getPrn() {
+        return prn;
+    }
+
+    public void setPrn(String prn) {
+        this.prn = prn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    public void setMarks(double marks) {
+        this.marks = marks;
+    }
+
+    // Display student details
+    public void displayStudent() {
+        System.out.println("PRN: " + prn + " | Name: " + name + " | DOB: " + dob + " | Marks: " + marks);
+    }
+}
